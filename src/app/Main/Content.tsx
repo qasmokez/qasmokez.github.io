f5c532949f4ee98d91b592d20fc17006eb4b070e
Jason Zhang <jasonz@eduroam-169-233-253-87.ucsc.edu>
2024-04-28 19:29:15 -0700

    Added responsive font size, avatar

--- src/app/Main/Content.tsx
+++ src/app/Main/Content.tsx
@@ -1,13 +1,26 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Divider from '@mui/material/Divider';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
 function Content() {
 
   return (
-    <Container maxWidth="xl">
+    <Container
+      maxWidth="xl"
+      sx={{mt: {xs: 3, sm: 7}}}
+    >
+      <Avatar
+        alt="Me"
+        src="https://media.licdn.com/dms/image/D5603AQFn5Pkz21Wcug/profile-displayphoto-shrink_400_400/0/1700704826162?e=1720051200&v=beta&t=O7zZ2C-aAZkLbHhhizPe4oX2BcdDZRxiHxCZWzKvc50"
+        sx={{
+          width: {xs: 60, sm: 90},
+          height: {xs: 60, sm: 90},
+        }}
+      />
       <Box
         sx={{
           display: 'flex',
@@ -17,15 +30,20 @@ function Content() {
             width: '100%',
             height: 128,
             fontWeight: 100,
+            mt: {xs: 2, sm: 5},
           },
         }}
       >
-        <Typography variant="h2" gutterBottom>
+        <Typography
+          sx={{typography: {xs: 'h4', sm: 'h2'}}}
+          gutterBottom
+        >
           Hi, I'm Jason
           <br />
           a CS student with a knack for innovation. This is my digital playground.
         </Typography>
       </Box>
+      <Divider />
     </Container>
   )
 }
